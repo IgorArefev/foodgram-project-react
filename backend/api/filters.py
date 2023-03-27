0e4f261b74abcf6dd07e1ad7d807508a17759087
Игорь Арефьев <djifrost@mail.ru>
2023-03-27 19:12:51 +0300

    v3

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -15,11 +15,7 @@ class IngredientSearchFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.CharFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all()
-    )
+    tags = filters.CharFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
