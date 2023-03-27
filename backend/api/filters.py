f58a3e19fc8467e42f3ae1884341e0e8a61c74e9
Игорь Арефьев <djifrost@mail.ru>
2023-03-27 19:26:50 +0300

    v3

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -15,7 +15,6 @@ class IngredientSearchFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    author = filters.NumberFilter(field_name='author__id')
     tags = filters.CharFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
