737b7645d6e766b74756b1ba5ce7ba77ad80880b
Игорь Арефьев <djifrost@mail.ru>
2023-03-27 21:34:14 +0300

    v3

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,10 +1,9 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
 
 FILTER_USER = {'favorites': 'favorites__user',
-               'shop_list': 'shop_list__user',
-               'tags': 'tags__slug'}
+               'shop_list': 'shop_list__user'}
 
 
 class IngredientSearchFilter(FilterSet):
@@ -16,7 +15,9 @@ class IngredientSearchFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.CharFilter(method='tags__slug')
+    tags = filters.ModelChoiceFilter(
+        queryset=Tag.objects.all()
+    )
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
@@ -24,15 +25,12 @@ class RecipeFilter(FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ('tags', 'is_favorited', 'is_in_shopping_cart')
 
     def _get_queryset(self, queryset, name, value, model):
         if value:
             return queryset.filter(**{FILTER_USER[model]: self.request.user})
         return queryset
-
-    def filter_tags(self, queryset, name, value):
-        return self._get_queryset(queryset, name, value, 'tags')
 
     def filter_is_favorited(self, queryset, name, value):
         return self._get_queryset(queryset, name, value, 'favorites')
